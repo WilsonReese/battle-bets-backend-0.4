3192d900faae7c69be5899122e72145241ba7273
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-07-19 22:35:32 +0000

    tested login and logout and it worked, just have to remember how to use Postman

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,8 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'Logged in successfully.' }, status: :ok
+    token = request.env['warden-jwt_auth.token']
+    render json: { message: 'Logged in successfully.' }, status: :ok, headers: { 'Authorization': "Bearer #{token}" }
   end
 
   def respond_to_on_destroy
