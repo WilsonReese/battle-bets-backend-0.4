45daf5cca38c8b1ef1c633477a79ac7f96687a4c
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-09-04 23:03:50 +0000

    updated bet options index -- not sure I needed to do this

--- app/controllers/bet_options_controller.rb
+++ app/controllers/bet_options_controller.rb
@@ -1,6 +1,14 @@
 class BetOptionsController < ApplicationController
+    before_action :set_game
+
     def index
-        @bet_options = BetOption.all
-        render json: @bet_options
+      @bet_options = @game.bet_options
+      render json: @bet_options
+    end
+  
+    private
+  
+    def set_game
+      @game = Game.find(params[:game_id])
     end
 end
