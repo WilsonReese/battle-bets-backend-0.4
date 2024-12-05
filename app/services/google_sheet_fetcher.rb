8a5676e894b66222f2e7a8b5629dd4c310e309d2
Reese Wilson <reese@chicagobooth.edu>
2024-12-05 11:37:20 -0600

    updated google sheets fetcher to use environment variables

--- app/services/google_sheet_fetcher.rb
+++ app/services/google_sheet_fetcher.rb
@@ -8,8 +8,18 @@ class GoogleSheetFetcher
   def initialize(sheet_id)
     @sheet_id = sheet_id
     @service = SHEETS::SheetsService.new
+
+    if Rails.env.production? || Rails.env.staging?
+      # Use environment variable for Heroku
+      credentials_json = Base64.decode64(ENV['GOOGLE_SHEETS_CREDENTIALS_BASE64'])
+      credentials_io = StringIO.new(credentials_json)
+    else
+      credentials_path = Rails.root.join('config', 'battle-bets-sheets-api-f42d6b61d979.json')
+      credentials_io = File.open(credentials_path)
+    end
+    
     @service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open('config/battle-bets-sheets-api-f42d6b61d979.json'),
+      json_key_io: credentials_io,
       scope: SCOPE
     )
   end
