5aeb550baf0ea141f3c97b1d4e6db4258d01826f
Reese Wilson <reese@chicagobooth.edu>
2025-05-15 13:09:37 -0500

    changed json response to simple html response

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,7 +6,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
     if resource.errors.empty?
-      render json: { message: "Your email address has been successfully confirmed." }, status: :ok
+      render html: "<h1 style='text-align:center; margin-top:30px;'>Your account has been confirmed.</h1>".html_safe, status: :ok
     else
       render html: "<h1 style='text-align:center; margin-top:30px;'>#{resource.errors.full_messages.join(", ")}</h1>".html_safe
     end
