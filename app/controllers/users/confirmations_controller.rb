1202b63e7b6086945c3af7e5b638e5ddceb94306
Reese Wilson <reese@battlebets.app>
2025-07-05 22:39:49 -0500

    got confirmation page to look better and to use the custom domain

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,10 +5,12 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
-    if resource.errors.empty?
-      render html: "<h1 style='margin-top:30px;'>Your account has been confirmed.</h1>".html_safe, status: :ok
-    else
-      render html: "<h1 style='margin-top:30px;'>#{resource.errors.full_messages.join(", ")}</h1>".html_safe
-    end
+  #   if resource.errors.empty?
+  #     render html: "<h1 style='margin-top:30px;'>Your account has been confirmed.</h1>".html_safe, status: :ok
+  #   else
+  #     render html: "<h1 style='margin-top:30px;'>#{resource.errors.full_messages.join(", ")}</h1>".html_safe
+  #   end
+  # end
+  render :show
   end
 end
