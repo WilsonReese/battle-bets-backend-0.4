b8ab9c5755a7cbc77bf54374acfe571dc954a061
Reese Wilson <reese@battlebets.app>
2025-08-09 12:54:57 -0500

    removing google sheets connection

--- app/services/google_sheet_fetcher.rb
+++ app/services/google_sheet_fetcher.rb
@@ -1,31 +1,31 @@
-require 'google/apis/sheets_v4'
-require 'googleauth'
+# require 'google/apis/sheets_v4'
+# require 'googleauth'
 
-class GoogleSheetFetcher
-  SHEETS = Google::Apis::SheetsV4
-  SCOPE = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+# class GoogleSheetFetcher
+#   SHEETS = Google::Apis::SheetsV4
+#   SCOPE = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-  def initialize(sheet_id)
-    @sheet_id = sheet_id
-    @service = SHEETS::SheetsService.new
+#   def initialize(sheet_id)
+#     @sheet_id = sheet_id
+#     @service = SHEETS::SheetsService.new
 
-    if Rails.env.production? || Rails.env.staging?
-      # Use environment variable for Heroku
-      credentials_json = Base64.decode64(ENV['GOOGLE_SHEETS_CREDENTIALS_BASE64'])
-      credentials_io = StringIO.new(credentials_json)
-    else
-      credentials_path = Rails.root.join('config', 'battle-bets-sheets-api-f42d6b61d979.json')
-      credentials_io = File.open(credentials_path)
-    end
+#     if Rails.env.production? || Rails.env.staging?
+#       # Use environment variable for Heroku
+#       credentials_json = Base64.decode64(ENV['GOOGLE_SHEETS_CREDENTIALS_BASE64'])
+#       credentials_io = StringIO.new(credentials_json)
+#     else
+#       credentials_path = Rails.root.join('config', 'battle-bets-sheets-api-f42d6b61d979.json')
+#       credentials_io = File.open(credentials_path)
+#     end
     
-    @service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: credentials_io,
-      scope: SCOPE
-    )
-  end
+#     @service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+#       json_key_io: credentials_io,
+#       scope: SCOPE
+#     )
+#   end
 
-  def fetch_data(tab_name, range = 'A:Z')
-    response = @service.get_spreadsheet_values(@sheet_id, "#{tab_name}!#{range}")
-    response.values
-  end
-end
+#   def fetch_data(tab_name, range = 'A:Z')
+#     response = @service.get_spreadsheet_values(@sheet_id, "#{tab_name}!#{range}")
+#     response.values
+#   end
+# end
--- lib/tasks/evaluate_google_sheet_bet_options.rake
+++ lib/tasks/evaluate_google_sheet_bet_options.rake
@@ -1,15 +1,15 @@
-namespace :google_sheet do
-  desc "Evaluate and update bet options from Google Sheets"
-  task evaluate: :environment do
-    sheet_id = '1OwUe9d9VEP032f8TOnOvqrbVUsDq4O6kS4PWQ4phtZ8'
-    fetcher = GoogleSheetFetcher.new(sheet_id)
+# namespace :google_sheet do
+#   desc "Evaluate and update bet options from Google Sheets"
+#   task evaluate: :environment do
+#     sheet_id = '1OwUe9d9VEP032f8TOnOvqrbVUsDq4O6kS4PWQ4phtZ8'
+#     fetcher = GoogleSheetFetcher.new(sheet_id)
 
-    puts "Fetching games data for evaluation."
-    games_data = fetcher.fetch_data('Games', 'A:D')
-    puts "Fetching bet options data for evaluation."
-    bet_options_data = fetcher.fetch_data('Bet Options', 'A:E')
+#     puts "Fetching games data for evaluation."
+#     games_data = fetcher.fetch_data('Games', 'A:D')
+#     puts "Fetching bet options data for evaluation."
+#     bet_options_data = fetcher.fetch_data('Bet Options', 'A:E')
 
-    GoogleSheetImporter.update_bet_options_and_bets(games_data, bet_options_data)
-    puts "Bet options evaluated and updated successfully."
-  end
-end
+#     GoogleSheetImporter.update_bet_options_and_bets(games_data, bet_options_data)
+#     puts "Bet options evaluated and updated successfully."
+#   end
+# end
