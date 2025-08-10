1719c32584ec0ac3b74d87fb2083db1267df5dbb
Reese Wilson <reese@battlebets.app>
2025-08-09 22:24:33 -0500

    setting puma persistent timeout to 5 seconds

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,7 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-persistent_timeout 15
+persistent_timeout 5
 
 # Specifies that the worker count should equal the number of processors in production.
 
